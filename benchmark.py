6b05cc12f598d6f1af0af3064ff790e73f78f496
seang <seang@b426a367-1105-0410-b9ff-cdf4ab011145>
2007-11-16 22:27:49 +0000

    Use distributed data in benchmarking script and add the WorldMill equivalent code to the ogr.py block
    git-svn-id: http://svn.gispython.org/svn/gispy/WorldMill/trunk@950 b426a367-1105-0410-b9ff-cdf4ab011145

--- benchmark.py
+++ benchmark.py
@@ -3,8 +3,8 @@ import timeit
 import mill
 import ogr
 
-PATH = '/var/gis/data/world'
-NAME = 'world_borders'
+PATH = 'docs/data'
+NAME = 'test_uk'
 
 # WorldMill
 s = """
@@ -18,19 +18,28 @@ t = timeit.Timer(
     setup='from __main__ import mill, PATH, NAME'
     )
 print "WorldMill (Cython)"
-print "%.2f usec/pass" % (1000000 * t.timeit(number=10)/10)
+print "%.2f usec/pass" % (1000000 * t.timeit(number=100)/100)
 print
 
 # OGR
 s = """
 source = ogr.Open(PATH)
 layer = source.GetLayerByName(NAME)
+schema = []
+ldefn = layer.GetLayerDefn()
+for n in range(ldefn.GetFieldCount()):
+    fdefn = ldefn.GetFieldDefn(n)
+    schema.append((fdefn.name, fdefn.type))
 layer.ResetReading()
 while 1:
     feature = layer.GetNextFeature()
     if not feature:
         break
     id = feature.GetFID()
+    props = {}
+    for i in range(feature.GetFieldCount()):
+        props[schema[i][0]] = feature.GetField(i)
+    geometry = feature.GetGeometryRef()
     feature.Destroy()
 source.Destroy()
 """
@@ -39,5 +48,5 @@ t = timeit.Timer(
     stmt=s,
     setup='from __main__ import ogr, PATH, NAME'
     )
-print "%.2f usec/pass" % (1000000 * t.timeit(number=10)/10)
+print "%.2f usec/pass" % (1000000 * t.timeit(number=100)/100)
 
