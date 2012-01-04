a6f07d61a430d8decf9b1a2cd750f9c7520bb158
sgillies <sean.gillies@gmail.com>
2012-01-04 12:37:55 -0700

    Support for coordinate reference systems and more protection from null pointers.

--- src/fiona/ogrinit.pyx
+++ src/fiona/ogrinit.pyx
@@ -1,14 +1,32 @@
+# This module initializes the OGR/CPL environment
 
 import atexit
+import logging
+
+cdef extern from "cpl_error.h":
+    void    CPLSetErrorHandler (void *handler)
+    void *  CPLQuietErrorHandler
 
 cdef extern from "ogr_api.h":
     void OGRRegisterAll()
     void OGRCleanupAll()
 
+log = logging.getLogger("Fiona")
+
+
+# Write OGR errors to the Fiona log
+cdef void * errorHandler(eErrClass, int err_no, char *msg):
+    log.error("OGR Error %s: %s", err_no, msg)
+
+def setup():
+    CPLSetErrorHandler(<void *>errorHandler)
+    OGRRegisterAll()
+
 def cleanup():
     OGRCleanupAll()
+    CPLSetErrorHandler(NULL)
 
 atexit.register(cleanup)
+setup()
 
-OGRRegisterAll()
 
