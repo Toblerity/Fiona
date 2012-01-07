9ecf1e0d10a9d5705d274bb46adf1e70b69f6c4a
sgillies <sean.gillies@gmail.com>
2012-01-07 13:12:25 -0700

    New and improved setup.

--- setup.py
+++ setup.py
@@ -3,6 +3,36 @@ try:
 except ImportError:
     from distutils.core import setup
 from distutils.core import Extension
+import logging
+import subprocess
+
+logging.basicConfig()
+log = logging.getLogger()
+
+# By default we'll try to get options via gdal-config.
+# On systems without, options will need to be set in setup.cfg or on the
+# setup command line.
+include_dirs = []
+library_dirs = []
+libraries = []
+try:
+    gdal_config = "gdal-config"
+    with open("gdal-config.txt", "w") as gcfg:
+        subprocess.call([gdal_config, "--cflags"], stdout=gcfg)
+        subprocess.call([gdal_config, "--libs"], stdout=gcfg)
+    with open("gdal-config.txt", "r") as gcfg:
+        cflags = gcfg.readline().strip()
+        libs = gcfg.readline().strip()
+    for item in cflags.split():
+        if item.startswith("-I"):
+            include_dirs.extend(item[2:].split(":"))
+    for item in libs.split():
+        if item.startswith("-L"):
+            library_dirs.extend(item[2:].split(":"))
+        elif item.startswith("-l"):
+            libraries.append(item[2:])
+except Exception, e:
+    log.error("Failed to get options via gdal-config: %s", str(e))
 
 # Get text from README.txt
 readme_text = file('README.rst', 'rb').read()
@@ -13,21 +43,31 @@ setup(name          = 'Fiona',
       license       = 'BSD',
       keywords      = 'gis vector feature data',
       author        = 'Sean Gillies',
-      author_email  = 'sgillies@frii.com',
+      author_email  = 'sean.gillies@gmail.com',
       maintainer        = 'Sean Gillies',
-      maintainer_email  = 'sgillies@frii.com',
-      url   = 'http://github.com/sgillies/fiona',
+      maintainer_email  = 'sean.gillies@gmail.com',
+      url   = 'http://github.com/Toblerity/Fiona',
       long_description = readme_text,
       package_dir = {'': 'src'},
       packages = ['fiona'],
       install_requires  = [],
       tests_require = ['nose'],
       ext_modules = [
-        Extension('fiona.ogrinit', ['src/fiona/ogrinit.c']),
-        Extension('fiona.ogrext', ['src/fiona/ogrext.c']),
+        Extension(
+            'fiona.ogrinit', 
+            ['src/fiona/ogrinit.c'],
+            include_dirs=include_dirs,
+            library_dirs=library_dirs,
+            libraries=libraries ),
+        Extension(
+            'fiona.ogrext', 
+            ['src/fiona/ogrext.c'],
+            include_dirs=include_dirs,
+            library_dirs=library_dirs,
+            libraries=libraries ),
         ],
       classifiers   = [
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
