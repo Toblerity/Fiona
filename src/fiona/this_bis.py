c995846c3fe4b00bf5b46793832ee87c4d9bc1a2
sgillies <sean.gillies@gmail.com>
2012-03-06 21:47:51 -0700

    Record types, not types.

--- src/fiona/this_bis.py
+++ src/fiona/this_bis.py
@@ -4,7 +4,7 @@ s = """The Zen of Python Vector Data Processing, by Sean Gillies
 Files are just files.
 A record that's read stays read.
 Objects are good, but data is better.
-Types aren't special enough to break the rules.
+Record types aren't special enough to break the rules.
 Functional programming is a great idea -- let's do more of that!"""
 
 print s
