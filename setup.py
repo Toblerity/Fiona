5ca1bd5daf09e9436c32f2e191a5f58235f89598
sgillies <sean.gillies@gmail.com>
2013-04-30 14:07:35 -0600

    Add Python 3 classifier.

--- setup.py
+++ setup.py
@@ -113,6 +113,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Topic :: Scientific/Engineering :: GIS',
     ],
 )
