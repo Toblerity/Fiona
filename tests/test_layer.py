2c05c4b0f21ff8fa828f681dad5bbb5a458782c8
sgillies <sean.gillies@gmail.com>
2013-05-24 18:07:43 -0500

    Multi-layer file and virtual filesystem support.
    A layer of a multi-layer file can be singled out by name.
    Apache Commons VFS strings (URI-ish) parameterize the virtual
    filesystems, these are converted to OGR's vsizip and vsitar
    paths.
    
    Found GDAL bug http://trac.osgeo.org/gdal/ticket/5093 in the
    process.

---
+++ tests/test_layer.py
@@ -0,0 +1,49 @@
+import logging
+import os
+import shutil
+import sys
+import unittest
+
+import fiona
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+from .test_collection import ReadingTest
+
+class FileReadingTest(ReadingTest):
+    
+    def setUp(self):
+        self.c = fiona.open("docs/data/test_uk.shp", "r", name="test_uk")
+    
+    def tearDown(self):
+        self.c.close()
+
+class DirReadingTest(ReadingTest):
+    
+    def setUp(self):
+        self.c = fiona.open("docs/data", "r", name="test_uk")
+    
+    def tearDown(self):
+        self.c.close()
+
+    def test_open_repr(self):
+        self.failUnlessEqual(
+            repr(self.c),
+            ("<open Collection 'docs/data:test_uk', mode 'r' "
+            "at %s>" % hex(id(self.c))))
+
+    def test_closed_repr(self):
+        self.c.close()
+        self.failUnlessEqual(
+            repr(self.c),
+            ("<closed Collection 'docs/data:test_uk', mode 'r' "
+            "at %s>" % hex(id(self.c))))
+
+    def test_path(self):
+        self.failUnlessEqual(self.c.path, "docs/data")
+
+class InvalidLayerTest(unittest.TestCase):
+
+    def test_invalid(self):
+        self.assertRaises(ValueError, fiona.open, ("docs/data/test_uk.shp"), name="foo")
+
---
+++ tests/test_vfs.py
@@ -0,0 +1,84 @@
+import logging
+import os
+import shutil
+import sys
+import unittest
+
+import fiona
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+from .test_collection import ReadingTest
+
+class ZipReadingTest(ReadingTest):
+    
+    def setUp(self):
+        self.c = fiona.open("zip://docs/data/test_uk.zip", "r")
+    
+    def tearDown(self):
+        self.c.close()
+
+    def test_open_repr(self):
+        self.failUnlessEqual(
+            repr(self.c),
+            ("<open Collection '/vsizip/docs/data/test_uk.zip:test_uk', mode 'r' "
+            "at %s>" % hex(id(self.c))))
+
+    def test_closed_repr(self):
+        self.c.close()
+        self.failUnlessEqual(
+            repr(self.c),
+            ("<closed Collection '/vsizip/docs/data/test_uk.zip:test_uk', mode 'r' "
+            "at %s>" % hex(id(self.c))))
+
+    def test_path(self):
+        self.failUnlessEqual(self.c.path, '/vsizip/docs/data/test_uk.zip')
+
+class ZipArchiveReadingTest(ReadingTest):
+    
+    def setUp(self):
+        self.c = fiona.open("/test_uk.shp", "r", vfs="zip://docs/data/test_uk.zip")
+    
+    def tearDown(self):
+        self.c.close()
+
+    def test_open_repr(self):
+        self.failUnlessEqual(
+            repr(self.c),
+            ("<open Collection '/vsizip/docs/data/test_uk.zip/test_uk.shp:test_uk', mode 'r' "
+            "at %s>" % hex(id(self.c))))
+
+    def test_closed_repr(self):
+        self.c.close()
+        self.failUnlessEqual(
+            repr(self.c),
+            ("<closed Collection '/vsizip/docs/data/test_uk.zip/test_uk.shp:test_uk', mode 'r' "
+            "at %s>" % hex(id(self.c))))
+
+    def test_path(self):
+        self.failUnlessEqual(self.c.path, '/vsizip/docs/data/test_uk.zip/test_uk.shp')
+
+class TarArchiveReadingTest(ReadingTest):
+    
+    def setUp(self):
+        self.c = fiona.open("/testing/test_uk.shp", "r", vfs="tar://docs/data/test_uk.tar")
+    
+    def tearDown(self):
+        self.c.close()
+
+    def test_open_repr(self):
+        self.failUnlessEqual(
+            repr(self.c),
+            ("<open Collection '/vsitar/docs/data/test_uk.tar/testing/test_uk.shp:test_uk', mode 'r' "
+            "at %s>" % hex(id(self.c))))
+
+    def test_closed_repr(self):
+        self.c.close()
+        self.failUnlessEqual(
+            repr(self.c),
+            ("<closed Collection '/vsitar/docs/data/test_uk.tar/testing/test_uk.shp:test_uk', mode 'r' "
+            "at %s>" % hex(id(self.c))))
+
+    def test_path(self):
+        self.failUnlessEqual(self.c.path, '/vsitar/docs/data/test_uk.tar/testing/test_uk.shp')
+
