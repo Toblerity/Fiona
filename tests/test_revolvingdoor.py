7f9e9fb2dee9bbc3df40a0e36be5d3677d4b0f73
Sean Gillies <sean.gillies@gmail.com>
2014-02-10 20:57:47 -0700

    A write-read test to shake out file handle bugs.
    This one does not expose the bug found in rasterio. One difference
    between the two libs is that Fiona has Python classes backed by
    C extension classes and rasterio has just the C extensions classes.
    I'm not exactly sure what this means at deallocation time, but
    Fiona is less sensitive to "is NULL".

---
+++ tests/test_revolvingdoor.py
@@ -0,0 +1,37 @@
+# Test of opening and closing and opening
+
+import logging
+import os.path
+import shutil
+import subprocess
+import sys
+import tempfile
+import unittest
+
+import fiona
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+log = logging.getLogger('fiona.tests')
+
+class RevolvingDoorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.tempdir = tempfile.mkdtemp()
+    
+    def tearDown(self):
+        shutil.rmtree(self.tempdir)
+
+    def test_write_revolving_door(self):
+
+        with fiona.open('docs/data/test_uk.shp') as src:
+            meta = src.meta
+            features = list(src)
+
+        shpname = os.path.join(self.tempdir, 'foo.shp')
+        
+        with fiona.open(shpname, 'w', **meta) as dst:
+            dst.writerecords(features)
+
+        with fiona.open(shpname) as src:
+            pass
+
