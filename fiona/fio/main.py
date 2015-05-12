1b887383e0f15a3ace2d51fd2a1df908be353932
Kevin Wurster <wursterk@gmail.com>
2015-05-12 08:33:54 -0400

    Better error handling when loading plugins.

--- fiona/fio/main.py
+++ fiona/fio/main.py
@@ -2,6 +2,9 @@
 # main: loader of all the command entry points.
 
 
+import sys
+import traceback
+
 from pkg_resources import iter_entry_points
 
 from fiona.fio.cli import cli
@@ -21,4 +24,21 @@ from fiona.fio.cli import cli
 #       ...
 
 for entry_point in iter_entry_points('fiona.fio_commands'):
-    entry_point.load()
+    try:
+        entry_point.load()
+    except Exception:
+        # Catch this so a busted plugin doesn't take down the CLI.
+        # Handled by registering a stub that does nothing other than
+        # explain the error.
+        msg = (
+            "Warning: Plugin module could not be loaded. Contact "
+            "its author for help.\n\n\b\n"
+            + traceback.format_exc())
+
+        short_msg = (
+            "Warning: Plugin module could not be loaded. See "
+            "`fio %s --help` for details." % entry_point.name)
+
+        @cli.command(entry_point.name, help=msg, short_help=short_msg)
+        def cmd_stub():
+            pass
