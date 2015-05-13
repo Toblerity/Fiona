184c074681b464dea35624fd640ea7d802e63737
Kevin Wurster <wursterk@gmail.com>
2015-05-12 21:17:36 -0400

    For #224 - switch to broken plugin implementation according to mapbox/rasterio@225179d30db07205108364c424d4094d61ebc4d3

--- fiona/fio/cli.py
+++ fiona/fio/cli.py
@@ -1,6 +1,11 @@
+"""Fiona's commandline interface core"""
+
+
 import json
 import logging
+import os
 import sys
+import traceback
 import warnings
 
 import click
@@ -10,6 +15,7 @@ from fiona import __version__ as fio_version
 
 
 warnings.simplefilter('default')
+
 
 def configure_logging(verbosity):
     log_level = max(10, 30 - 10*verbosity)
@@ -23,8 +29,61 @@ def print_version(ctx, param, value):
     ctx.exit()
 
 
+class BrokenCommand(click.Command):
+
+    """A dummy command that provides help for broken plugins."""
+
+    def __init__(self, name):
+        click.Command.__init__(self, name)
+        self.help = (
+            "Warning: entry point could not be loaded. Contact "
+            "its author for help.\n\n\b\n"
+            + traceback.format_exc())
+        self.short_help = (
+            "Warning: could not load plugin. See `fio %s --help`." % self.name)
+
+    def invoke(self, ctx):
+
+        """Print the error message instead of doing nothing."""
+
+        click.echo(self.help, color=ctx.color)
+        ctx.exit()
+
+
+class FioGroup(click.Group):
+    """Custom formatting for the commands of broken plugins."""
+
+    def format_commands(self, ctx, formatter):
+        """Extra format methods for multi methods that adds all the commands
+        after the options.
+        """
+
+        rows = []
+        for subcommand in self.list_commands(ctx):
+            cmd = self.get_command(ctx, subcommand)
+            # What is this, the tool lied about a command.  Ignore it
+            if cmd is None:
+                continue
+
+            help = cmd.short_help or ''
+
+            # Mark broken subcommands with a pile of poop.
+            name = cmd.name
+            if isinstance(cmd, BrokenCommand):
+                if os.environ.get('FIO_HONESTLY'):
+                    name += u'\U0001F4A9'
+                else:
+                    name += u'\u2020'
+
+            rows.append((name, help))
+
+        if rows:
+            with formatter.section('Commands'):
+                formatter.write_dl(rows)
+
+
 # The CLI command group.
-@click.group(help="Fiona command line interface.")
+@click.group(help="Fiona command line interface.", cls=FioGroup)
 @verbose_opt
 @quiet_opt
 @click.option('--version', is_flag=True, callback=print_version,
