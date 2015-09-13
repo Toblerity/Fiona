2b0bbd7143c7f539c2c9fffc1ba07f37dcec9847
Rene Buffat <buffat@gmail.com>
2015-09-13 22:16:07 +0200

    init travis gdal compile

---
+++ scripts/travis_gdal_install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -ex
+GDALDIR="${HOME}/gdalbuild"
+CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
+
+if [ ! -d "$GDALDIR" ]; then
+  mkdir $GDALDIR;
+fi
+
+if [ "$GDALVERSION" = "1.11.2" ]; then
+    cd $GDALDIR
+    if [ ! -d "$GDALDIR/gdal-1.11.2" ]; then
+      wget http://download.osgeo.org/gdal/1.11.2/gdal-1.11.2.tar.gz
+      tar -xzvf gdal-1.11.2.tar.gz
+    fi
+    cd gdal-1.11.2
+    ./configure --prefix=/usr && make -j $CORES && sudo make install
+fi
