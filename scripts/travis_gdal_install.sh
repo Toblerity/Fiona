9c80aa6600a53c8801bd6ea8d5aaf6dc47674777
Rene Buffat <buffat@gmail.com>
2015-09-14 19:06:32 +0200

    minimze gdal drivers to compile

--- scripts/travis_gdal_install.sh
+++ scripts/travis_gdal_install.sh
@@ -2,6 +2,50 @@
 set -ex
 GDALDIR="${HOME}/gdalbuild"
 
+GDALOPTS="--prefix=/usr --with-ogr \
+            --with-geos \
+            --without-libtool \
+            --with-libz=internal \
+            --with-libtiff=internal \
+            --with-geotiff=internal \
+            --without-gif \
+            --without-pg \
+            --without-grass \
+            --without-libgrass \
+            --without-cfitsio \
+            --without-pcraster \
+            --without-netcdf \
+            --without-png \
+            --without-jpeg \
+            --without-gif \
+            --without-ogdi \
+            --without-fme \
+            --without-hdf4 \
+            --without-hdf5 \
+            --without-jasper \
+            --without-ecw \
+            --without-kakadu \
+            --without-mrsid \
+            --without-jp2mrsid \
+            --without-bsb \
+            --without-grib \
+            --without-mysql \
+            --without-ingres \
+            --without-xerces \
+            --without-expat \
+            --without-odbc \
+            --without-curl \
+            --without-sqlite3 \
+            --without-dwgdirect \
+            --without-panorama \
+            --without-idb \
+            --without-sde \
+            --without-perl \
+            --without-php \
+            --without-ruby \
+            --without-python \
+            --without-ogpython"
+
 # Create build dir if not exists
 if [ ! -d "$GDALDIR" ]; then
   mkdir $GDALDIR;
@@ -15,7 +59,7 @@ if [ "$GDALVERSION" = "1.9.2" ]; then
       tar -xzvf gdal-1.9.2.tar.gz
     fi
     cd gdal-1.9.2
-    ./configure --prefix=/usr --without-ogdi && make -j 2 && sudo make install
+    ./configure $GDALOPTS && make -j 2 && sudo make install
 elif [ "$GDALVERSION" = "1.11.2" ]; then
     cd $GDALDIR
     if [ ! -d "$GDALDIR/gdal-1.11.2" ]; then
@@ -23,7 +67,7 @@ elif [ "$GDALVERSION" = "1.11.2" ]; then
       tar -xzvf gdal-1.11.2.tar.gz
     fi
     cd gdal-1.11.2
-    ./configure --prefix=/usr --without-ogdi && make -j 2 && sudo make install
+    ./configure $GDALOPTS && make -j 2 && sudo make install
 elif [ "$GDALVERSION" = "2.0.0" ]; then
     cd $GDALDIR
     if [ ! -d "$GDALDIR/gdal-2.0.0" ]; then
@@ -31,7 +75,7 @@ elif [ "$GDALVERSION" = "2.0.0" ]; then
       tar -xzvf gdal-2.0.0.tar.gz
     fi
     cd gdal-2.0.0
-    ./configure --prefix=/usr --without-ogdi && make -j 2 && sudo make install
+    ./configure $GDALOPTS && make -j 2 && sudo make install
 fi
 
 # change back to travis build dir
