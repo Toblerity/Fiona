f64182a5e43cf7343ce0b2444814f498265aec65
Joshua Arnott <josh@snorfalorpagus.net>
2015-10-28 23:21:34 +0000

    Fixed test_remove to work with nosetests

--- tests/test_remove.py
+++ tests/test_remove.py
@@ -2,6 +2,7 @@ import logging
 import sys
 import os
 
+import tempfile
 import pytest
 
 import fiona
@@ -10,7 +11,7 @@ import fiona
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 
-def create_test_data(filename, driver):
+def create_sample_data(filename, driver):
     meta = {
         'driver': driver,
         'schema': {
@@ -29,29 +30,36 @@ def create_test_data(filename, driver):
     assert(os.path.exists(filename))
 
 
-def test_remove(tmpdir):
-    filename_shp = str(tmpdir.join('test.shp'))
+def test_remove(tmpdir=None):
+    if tmpdir is None:
+        tmpdir = tempfile.mkdtemp()
+    filename_shp = os.path.join(tmpdir, 'test.shp')
     
-    create_test_data(filename_shp, driver='ESRI Shapefile')
+    create_sample_data(filename_shp, driver='ESRI Shapefile')
     fiona.remove(filename_shp, driver='ESRI Shapefile')
     assert(not os.path.exists(filename_shp))
     
     with pytest.raises(RuntimeError):
         fiona.remove(filename_shp, driver='ESRI Shapefile')
 
-def test_remove_driver(tmpdir):
-    filename_shp = str(tmpdir.join('test.shp'))
-    filename_json = str(tmpdir.join('test.json'))
-    
-    create_test_data(filename_shp, driver='ESRI Shapefile')
-    create_test_data(filename_json, driver='GeoJSON')
+def test_remove_driver(tmpdir=None):
+    if tmpdir is None:
+        tmpdir = tempfile.mkdtemp()
+    filename_shp = os.path.join(tmpdir, 'test.shp')
+    filename_json = os.path.join(tmpdir, 'test.json')
+        
+    create_sample_data(filename_shp, driver='ESRI Shapefile')
+    create_sample_data(filename_json, driver='GeoJSON')
     fiona.remove(filename_json, driver='GeoJSON')
     assert(not os.path.exists(filename_json))
     assert(os.path.exists(filename_shp))
 
-def test_remove_collection(tmpdir):
-    filename_shp = str(tmpdir.join('test.shp'))
-    create_test_data(filename_shp, driver='ESRI Shapefile')
+def test_remove_collection(tmpdir=None):
+    if tmpdir is None:
+        tmpdir = tempfile.mkdtemp()
+    filename_shp = os.path.join(tmpdir, 'test.shp')
+    
+    create_sample_data(filename_shp, driver='ESRI Shapefile')
     collection = fiona.open(filename_shp, 'r')
     fiona.remove(collection)
     assert(not os.path.exists(filename_shp))
