af768f0a25796f7e988afcf3be72be2ec2fe1160
Kevin Wurster <kevin@skytruth.org>
2016-04-11 23:41:47 -0400

    Enable logging in fio ls

--- fiona/fio/ls.py
+++ fiona/fio/ls.py
@@ -9,11 +9,15 @@ from cligj import indent_opt
 @click.command()
 @click.argument('input', type=click.Path(exists=True))
 @indent_opt
-def ls(input, indent):
+@click.pass_context
+def ls(ctx, input, indent):
 
     """
     List layers in a datasource.
     """
 
-    result = fiona.listlayers(input)
-    click.echo(json.dumps(result, indent=indent))
+    verbosity = (ctx.obj and ctx.obj['verbosity']) or 2
+
+    with fiona.drivers(CPL_DEBUG=verbosity > 2):
+        result = fiona.listlayers(input)
+        click.echo(json.dumps(result, indent=indent))
