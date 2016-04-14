d04d8b11149741c4fb83b355a7a28b715cf696ee
Kevin Wurster <wursterk@gmail.com>
2016-04-14 01:24:27 -0400

    Unittest for $ fio info verifying that --layer can be a name or index.

--- tests/test_fio_info.py
+++ tests/test_fio_info.py
@@ -40,6 +40,13 @@ def test_all_registered():
         assert ep.name in main_group.commands
 
 
+def _filter_info_warning(lines):
+    """$ fio info can issue a RuntimeWarning, but click adds stderr to stdout
+    so we have to filter it out before decoding JSON lines."""
+    lines = list(filter(lambda x: 'RuntimeWarning' not in x, lines))
+    return lines
+
+
 def test_info_no_count():
     """Make sure we can still get a `$ fio info` report on datasources that do
     not support feature counting, AKA `len(collection)`.
@@ -47,8 +54,20 @@ def test_info_no_count():
     runner = CliRunner()
     result = runner.invoke(main_group, ['info', 'tests/data/test_gpx.gpx'])
     assert result.exit_code == 0
-    lines = list(filter(
-        lambda x: 'RuntimeWarning' not in x,
-        result.output.splitlines()))
+    lines = _filter_info_warning(result.output.splitlines())
     assert len(lines) == 1, "First line is warning & second is JSON.  No more."
     assert json.loads(lines[0])['count'] is None
+
+
+def test_info_layer():
+    for layer in ('routes', '1'):
+        runner = CliRunner()
+        result = runner.invoke(main_group, [
+            'info',
+            'tests/data/test_gpx.gpx',
+            '--layer', layer])
+        print(result.output)
+        assert result.exit_code == 0
+        lines = _filter_info_warning(result.output.splitlines())
+        assert len(lines) == 1, "1st line is warning & 2nd is JSON - no more."
+        assert json.loads(lines[0])['name'] == 'routes'
