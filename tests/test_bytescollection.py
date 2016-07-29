9ef1e26c6a6616bc16d999c2d7efa9cba90a99ca
Kevin Wurster <kevin@skytruth.org>
2016-07-29 11:31:18 -0400

    Revert tests/test_bytescollection.py::ReadingTest to a unittest.TestCase() with pytest fixture wrappers to minimize test changes.

--- tests/test_bytescollection.py
+++ tests/test_bytescollection.py
@@ -1,4 +1,4 @@
-"""Tests for `fiona.collection.BytesCollection()`."""
+"""Tests for ``fiona.BytesCollection()``."""
 
 
 import sys
@@ -13,221 +13,192 @@ import fiona
 FIXME_WINDOWS = sys.platform.startswith('win')
 
 
-@pytest.fixture(scope='function')
-def bytecol(path_coutwildrnp_json, request):
-    """Return an open ``BytesCollection()`` which is automatically closed on
-    teardown."""
-    with open(path_coutwildrnp_json) as src:
-        bytesbuf = src.read().encode('utf-8')
-    bc = fiona.BytesCollection(bytesbuf)
-    def fin():
-        bc.close()
-    request.addfinalizer(fin)
-    return bc
-
-
-@pytest.mark.skipif(six.PY2, reason='string are bytes in Python 2')
-def test_construct_with_str(path_coutwildrnp_json):
-    with open(path_coutwildrnp_json) as src:
-        strbuf = src.read()
-    with pytest.raises(ValueError):
-        fiona.BytesCollection(strbuf)
-
-
-@pytest.mark.skipif(
-    FIXME_WINDOWS,
-    reason="FIXME on Windows. Please look into why this test is not working.")
-def test_open_repr(bytecol):
-    """Skip checking the name of the virtual file as it produced by uuid."""
-    assert repr(bytecol).startswith("<open BytesCollection '/vsimem/")
-    assert repr(bytecol).endswith(":OGRGeoJSON', mode 'r' at %s>" % hex(id(bytecol)))
-
-
-@pytest.mark.skipif(
-    FIXME_WINDOWS,
-    reason="FIXME on Windows. Please look into why this test is not working.")
-def test_closed_repr(bytecol):
-    """Skip checking the name of the virtual file as it produced by uuid."""
-    bytecol.close()
-    assert repr(bytecol).startswith("<closed BytesCollection '/vsimem/")
-    assert repr(bytecol).endswith(":OGRGeoJSON', mode 'r' at %s>" % hex(id(bytecol)))
-
-
-def test_path(bytecol):
-    assert bytecol.path == bytecol.virtual_file
-
-
-def test_closed_virtual_file(bytecol):
-    bytecol.close()
-    assert bytecol.virtual_file is None
-
-
-def test_closed_buf(bytecol):
-    bytecol.close()
-    assert bytecol.bytesbuf is None
-
-
-def test_name(bytecol):
-    assert bytecol.name == 'OGRGeoJSON'
-
-
-def test_mode(bytecol):
-    assert bytecol.mode == 'r'
-
-
-@pytest.mark.skipif(
-    FIXME_WINDOWS,
-    reason="FIXME on Windows. Please look into why this test is not working.")
-def test_collection(bytecol):
-    assert bytecol.encoding == 'utf-8'
-
-
-def test_iter(bytecol):
-    assert iter(bytecol)
-
-
-def test_closed_no_iter(bytecol):
-    bytecol.close()
-    with pytest.raises(ValueError):
-        iter(bytecol)
-
-
-def test_len(bytecol):
-    assert len(bytecol) == 67
-
-
-def test_closed_len(bytecol):
-    """Len is lazy, it's never computed in this case. TODO?"""
-    bytecol.close()
-    assert len(bytecol) == 0
-
-
-def test_len_closed_len(bytecol):
-    """Lazy len is computed in this case and sticks."""
-    len(bytecol)
-    bytecol.close()
-    assert len(bytecol) == 67
-
-
-def test_driver(bytecol):
-    assert bytecol.driver == "GeoJSON"
-
-
-def test_closed_driver(bytecol):
-    bytecol.close()
-    assert bytecol.driver is None
-
-
-def test_driver_closed_driver(bytecol):
-    bytecol.driver
-    bytecol.close()
-    assert bytecol.driver == "GeoJSON"
-
-
-def test_schema(bytecol):
-    s = bytecol.schema['properties']
-    assert s['PERIMETER'] == "float"
-    assert s['NAME'] == "str"
-    assert s['URL'] == "str"
-    assert s['STATE_FIPS'] == "str"
-    assert s['WILDRNP020'] == "int"
-
-
-def test_closed_schema(bytecol):
-    """Schema is lazy too, never computed in this case. TODO?"""
-    bytecol.close()
-    assert bytecol.schema is None
-
-
-def test_schema_closed_schema(bytecol):
-    bytecol.schema
-    bytecol.close()
-    assert sorted(bytecol.schema.keys()) == ['geometry', 'properties']
-
-
-def test_crs(bytecol):
-    assert bytecol.crs['init'] == 'epsg:4326'
-
-
-def test_crs_wkt(bytecol):
-    assert bytecol.crs_wkt.startswith('GEOGCS["WGS 84"')
-
-
-def test_closed_crs(bytecol):
-    """CRS is lazy too, never computed in this case. TODO?"""
-    bytecol.close()
-    assert bytecol.crs is None
-
-
-def test_crs_closed_crs(bytecol):
-    bytecol.crs
-    bytecol.close()
-    assert sorted(bytecol.crs.keys()) == ['init']
-
-
-def test_meta(bytecol):
-    assert sorted(bytecol.meta.keys()) == ['crs', 'crs_wkt', 'driver', 'schema']
-
-
-def test_bounds(bytecol):
-    assert round(bytecol.bounds[0], 6) == round(-113.564247, 6)
-    assert round(bytecol.bounds[1], 6) == round(37.068981, 6)
-    assert round(bytecol.bounds[2], 6) == round(-104.970871, 6)
-    assert round(bytecol.bounds[3], 6) == round(41.996277, 6)
-
-
-def test_iter_one(bytecol):
-    itr = iter(bytecol)
-    f = next(itr)
-    assert f['id'] == "0"
-    assert f['properties']['STATE'] == 'UT'
-
-
-def test_iter_list(bytecol):
-    f = list(bytecol)[0]
-    assert f['id'] == "0"
-    assert f['properties']['STATE'] == 'UT'
-
-
-def test_re_iter_list(bytecol):
-    f = list(bytecol)[0]  # Run through iterator
-    f = list(bytecol)[0]  # Run through a new, reset iterator
-    assert f['id'] == "0"
-    assert f['properties']['STATE'] == 'UT'
-
-
-def test_getitem_one(bytecol):
-    f = bytecol[0]
-    assert f['id'] == "0"
-    assert f['properties']['STATE'] == 'UT'
-
-
-def test_no_write(bytecol):
-    with pytest.raises(IOError):
-        bytecol.write({})
-
-
-def test_iter_items_list(bytecol):
-    i, f = list(bytecol.items())[0]
-    assert i == 0
-    assert f['id'] == "0"
-    assert f['properties']['STATE'] == 'UT'
-
-
-def test_iter_keys_list(bytecol):
-    i = list(bytecol.keys())[0]
-    assert i == 0
-
-
-def test_in_keys(bytecol):
-    assert 0 in bytecol.keys()
-    assert 0 in bytecol
-
-
+@pytest.mark.usefixtures('uttc_path_coutwildrnp_json')
+class ReadingTest(unittest.TestCase):
+
+    def setUp(self):
+        with open(self.path_coutwildrnp_json) as src:
+            bytesbuf = src.read().encode('utf-8')
+        self.c = fiona.BytesCollection(bytesbuf)
+
+    def tearDown(self):
+        self.c.close()
+
+    @pytest.mark.skipif(six.PY2, reason='string are bytes in Python 2')
+    def test_construct_with_str(self):
+        with open(self.path_coutwildrnp_json) as src:
+            strbuf = src.read()
+        self.assertRaises(ValueError, fiona.BytesCollection, strbuf)
+
+    @pytest.mark.skipif(
+        FIXME_WINDOWS,
+        reason="FIXME on Windows. Please look into why this test is not working.")
+    def test_open_repr(self):
+        # I'm skipping checking the name of the virtual file as it produced by uuid.
+        self.assertTrue(repr(self.c).startswith("<open BytesCollection '/vsimem/"))
+        self.assertTrue(repr(self.c).endswith(":OGRGeoJSON', mode 'r' at %s>" % hex(id(self.c))))
+
+    @pytest.mark.skipif(
+        FIXME_WINDOWS,
+        reason="FIXME on Windows. Please look into why this test is not working.")
+    def test_closed_repr(self):
+        # I'm skipping checking the name of the virtual file as it produced by uuid.
+        self.c.close()
+        self.assertTrue(repr(self.c).startswith("<closed BytesCollection '/vsimem/"))
+        self.assertTrue(repr(self.c).endswith(":OGRGeoJSON', mode 'r' at %s>" % hex(id(self.c))))
+
+    def test_path(self):
+        self.assertEqual(self.c.path, self.c.virtual_file)
+
+    def test_closed_virtual_file(self):
+        self.c.close()
+        self.assertTrue(self.c.virtual_file is None)
+
+    def test_closed_buf(self):
+        self.c.close()
+        self.assertTrue(self.c.bytesbuf is None)
+
+    def test_name(self):
+        self.assertEqual(self.c.name, 'OGRGeoJSON')
+
+    def test_mode(self):
+        self.assertEqual(self.c.mode, 'r')
+
+    @pytest.mark.skipif(
+        FIXME_WINDOWS,
+        reason="FIXME on Windows. Please look into why this test is not working.")
+    def test_collection(self):
+        self.assertEqual(self.c.encoding, 'utf-8')
+
+    def test_iter(self):
+        self.assertTrue(iter(self.c))
+
+    def test_closed_no_iter(self):
+        self.c.close()
+        self.assertRaises(ValueError, iter, self.c)
+
+    def test_len(self):
+        self.assertEqual(len(self.c), 67)
+
+    def test_closed_len(self):
+        # Len is lazy, it's never computed in this case. TODO?
+        self.c.close()
+        self.assertEqual(len(self.c), 0)
+
+    def test_len_closed_len(self):
+        # Lazy len is computed in this case and sticks.
+        len(self.c)
+        self.c.close()
+        self.assertEqual(len(self.c), 67)
+
+    def test_driver(self):
+        self.assertEqual(self.c.driver, "GeoJSON")
+
+    def test_closed_driver(self):
+        self.c.close()
+        self.assertEqual(self.c.driver, None)
+
+    def test_driver_closed_driver(self):
+        self.c.driver
+        self.c.close()
+        self.assertEqual(self.c.driver, "GeoJSON")
+
+    def test_schema(self):
+        s = self.c.schema['properties']
+        self.assertEqual(s['PERIMETER'], "float")
+        self.assertEqual(s['NAME'], "str")
+        self.assertEqual(s['URL'], "str")
+        self.assertEqual(s['STATE_FIPS'], "str")
+        self.assertEqual(s['WILDRNP020'], "int")
+
+    def test_closed_schema(self):
+        # Schema is lazy too, never computed in this case. TODO?
+        self.c.close()
+        self.assertEqual(self.c.schema, None)
+
+    def test_schema_closed_schema(self):
+        self.c.schema
+        self.c.close()
+        self.assertEqual(
+            sorted(self.c.schema.keys()),
+            ['geometry', 'properties'])
+
+    def test_crs(self):
+        crs = self.c.crs
+        self.assertEqual(crs['init'], 'epsg:4326')
+
+    def test_crs_wkt(self):
+        crs = self.c.crs_wkt
+        self.assertTrue(crs.startswith('GEOGCS["WGS 84"'))
+
+    def test_closed_crs(self):
+        # Crs is lazy too, never computed in this case. TODO?
+        self.c.close()
+        self.assertEqual(self.c.crs, None)
+
+    def test_crs_closed_crs(self):
+        self.c.crs
+        self.c.close()
+        self.assertEqual(
+            sorted(self.c.crs.keys()),
+            ['init'])
+
+    def test_meta(self):
+        self.assertEqual(
+            sorted(self.c.meta.keys()),
+            ['crs', 'crs_wkt', 'driver', 'schema'])
+
+    def test_bounds(self):
+        self.assertAlmostEqual(self.c.bounds[0], -113.564247, 6)
+        self.assertAlmostEqual(self.c.bounds[1], 37.068981, 6)
+        self.assertAlmostEqual(self.c.bounds[2], -104.970871, 6)
+        self.assertAlmostEqual(self.c.bounds[3], 41.996277, 6)
+
+    def test_iter_one(self):
+        itr = iter(self.c)
+        f = next(itr)
+        self.assertEqual(f['id'], "0")
+        self.assertEqual(f['properties']['STATE'], 'UT')
+
+    def test_iter_list(self):
+        f = list(self.c)[0]
+        self.assertEqual(f['id'], "0")
+        self.assertEqual(f['properties']['STATE'], 'UT')
+
+    def test_re_iter_list(self):
+        f = list(self.c)[0] # Run through iterator
+        f = list(self.c)[0] # Run through a new, reset iterator
+        self.assertEqual(f['id'], "0")
+        self.assertEqual(f['properties']['STATE'], 'UT')
+
+    def test_getitem_one(self):
+        f = self.c[0]
+        self.assertEqual(f['id'], "0")
+        self.assertEqual(f['properties']['STATE'], 'UT')
+
+    def test_no_write(self):
+        self.assertRaises(IOError, self.c.write, {})
+
+    def test_iter_items_list(self):
+        i, f = list(self.c.items())[0]
+        self.assertEqual(i, 0)
+        self.assertEqual(f['id'], "0")
+        self.assertEqual(f['properties']['STATE'], 'UT')
+
+    def test_iter_keys_list(self):
+        i = list(self.c.keys())[0]
+        self.assertEqual(i, 0)
+
+    def test_in_keys(self):
+        self.assertTrue(0 in self.c.keys())
+        self.assertTrue(0 in self.c)
+
+
+@pytest.mark.usefixtures('uttc_coutwildrnp_json')
 class FilterReadingTest(unittest.TestCase):
 
     def setUp(self):
-        with open('tests/data/coutwildrnp.json') as src:
+        with open(self.path_coutwildrnp_json) as src:
             bytesbuf = src.read().encode('utf-8')
         self.c = fiona.BytesCollection(bytesbuf)
 
