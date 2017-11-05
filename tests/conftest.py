6e7e91bfb1e5043f14fe0513c0be58771bdb99b9
Joshua Arnott <josh@snorfalorpagus.net>
2017-11-05 21:41:32 +0000

    GDAL 2.3.x silently converts ESRI WKT to OGC WKT. We need to handle either in tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -181,3 +181,9 @@ def uttc_path_gpx(path_gpx, request):
     """Make the ``path_gpx`` fixture work with a ``unittest.TestCase()``.
     ``uttc`` stands for unittest test case."""
     request.cls.path_gpx = path_gpx
+
+"""
+GDAL 2.3.x silently converts ESRI WKT to OGC WKT
+The regular expression below will match against either
+"""
+WGS84PATTERN = 'GEOGCS\["(?:GCS_WGS_1984|WGS 84)",DATUM\["WGS_1984",SPHEROID\["WGS_84"'
