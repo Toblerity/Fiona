ee4b2a3f3a7a76e845e2befaea6c256bb0844311
Patrick M Young <patrick.mckendree.young@gmail.com>
2017-11-06 22:12:21 -0700

    Appveyor reporting boto3 session is also None.

--- fiona/drvsupport.py
+++ fiona/drvsupport.py
@@ -158,14 +158,15 @@ class AWSGDALEnv(GDALEnv):
     def __init__(self, **options):
         import boto3
         session = boto3.Session()
-        creds = session.get_credentials()
-        if creds:
-            if creds.access_key:
-                options.update(aws_access_key_id=creds.access_key)
-            if creds.secret_key:  # pragma: no branch
-                options.update(aws_secret_access_key=creds.secret_key)
-            if creds.token:
-                options.update(aws_session_token=creds.token)
-        if session.region_name:
-            options.update(aws_region=session.region_name)
+        if session:
+            if session.region_name:
+                options.update(aws_region=session.region_name)
+            creds = session.get_credentials()
+            if creds:
+                if creds.access_key:
+                    options.update(aws_access_key_id=creds.access_key)
+                if creds.secret_key:  # pragma: no branch
+                    options.update(aws_secret_access_key=creds.secret_key)
+                if creds.token:
+                    options.update(aws_session_token=creds.token)
         super(AWSGDALEnv, self).__init__(**options)
