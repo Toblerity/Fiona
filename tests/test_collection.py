5e116e62f2bdf268df20b28c0935d927aeb1753b
Joshua Arnott <josh@snorfalorpagus.net>
2018-01-18 20:17:32 +0000

    Use context manager in ignore fields tests

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -282,23 +282,24 @@ class ReadingTest(unittest.TestCase):
 class IgnoreFieldsTest(unittest.TestCase):
 
     def test_without_ignore(self):
-        collection = fiona.open(WILDSHP, "r")
-        assert("AREA" in collection.schema["properties"].keys())
-        feature = next(collection)
-        assert(feature["properties"]["AREA"] is not None)
-        assert(feature["properties"]["STATE"] is not None)
-        assert(feature["properties"]["NAME"] is not None)
+        with fiona.open(WILDSHP, "r") as collection:
+            assert("AREA" in collection.schema["properties"].keys())
+            feature = next(collection)
+            assert(feature["properties"]["AREA"] is not None)
+            assert(feature["properties"]["STATE"] is not None)
+            assert(feature["properties"]["NAME"] is not None)
 
     def test_ignore_fields(self):
-        collection = fiona.open(WILDSHP, "r", ignore_fields=["AREA", "STATE"])
-        assert("AREA" in collection.schema["properties"].keys())
-        feature = next(collection)
-        assert(feature["properties"]["AREA"] is None)
-        assert(feature["properties"]["STATE"] is None)
-        assert(feature["properties"]["NAME"] is not None)
+        with fiona.open(WILDSHP, "r", ignore_fields=["AREA", "STATE"]) as collection:
+            assert("AREA" in collection.schema["properties"].keys())
+            feature = next(collection)
+            assert(feature["properties"]["AREA"] is None)
+            assert(feature["properties"]["STATE"] is None)
+            assert(feature["properties"]["NAME"] is not None)
 
     def test_ignore_invalid_field_missing(self):
-        collection = fiona.open(WILDSHP, "r", ignore_fields=["DOES_NOT_EXIST"])
+        with fiona.open(WILDSHP, "r", ignore_fields=["DOES_NOT_EXIST"]) as collection:
+            pass
 
     def test_ignore_invalid_field_not_string(self):
         self.assertRaises(
