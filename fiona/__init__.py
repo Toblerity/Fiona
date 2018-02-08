390a0cb70328328de608fa9acb302e11fbf4f00f
Sean C. Gillies <sean@mapbox.com>
2018-02-08 14:12:14 -0700

    Post-release notes and version

--- fiona/__init__.py
+++ fiona/__init__.py
@@ -81,7 +81,7 @@ import uuid
 
 
 __all__ = ['bounds', 'listlayers', 'open', 'prop_type', 'prop_width']
-__version__ = "1.7.11"
+__version__ = "1.7.11.post1"
 __gdal_version__ = get_gdal_release_name().decode('utf-8')
 
 log = logging.getLogger(__name__)
