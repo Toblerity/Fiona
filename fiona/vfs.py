Toblerity/Fiona

commit f70b44c512076961cd52c698558028f46d7bdd1a

Gdal23 fixes (#576)
* remove silent option for gdal make as it might lead to timeouts on travis

* skip test_grenada_bytes_geojson for gdal >= 2.3.0

* missing import

* switch to get_gdal_version_num

* cleanup

* print also gdal version number

* pytest strcit

* missing @

* pytest --strict requires markes to be registred

* add scheme to path

* move svn.osgeo.org to github

* wrong url change

* case destinction remote schemes

fiona/vfs.py
@@ -57,7 +57,11 @@ def parse_paths(uri, vfs=None):
         scheme = parts.scheme
         path = parts.path
         if parts.netloc and parts.netloc != 'localhost':
-            path = parts.netloc + path
+            if scheme.split("+")[-1] in REMOTESCHEMES:
+                # We need to deal with cases such as zip+https://server.com/data.zip
+                path = "{}://{}{}".format(scheme.split("+")[-1], parts.netloc, path)
+            else:
+                path = parts.netloc + path
         if scheme in SCHEMES:
             parts = path.split('!')
             path = parts.pop() if parts else None

scripts/travis_gdal_install.sh
@@ -58,7 +58,7 @@ if [ "$GDALVERSION" = "trunk" ]; then
   git clone -b master --single-branch --depth=1 https://github.com/OSGeo/gdal.git $GDALBUILD/trunk
   cd $GDALBUILD/trunk/gdal
   ./configure --prefix=$GDALINST/gdal-$GDALVERSION $GDALOPTS
-  make -s -j 2
+  make -j 2
   make install
 elif [ ! -d "$GDALINST/gdal-$GDALVERSION" ]; then
   # only build if not already installed
@@ -67,7 +67,7 @@ elif [ ! -d "$GDALINST/gdal-$GDALVERSION" ]; then
   tar -xzf gdal-$GDALVERSION.tar.gz
   cd gdal-$GDALVERSION
   ./configure --prefix=$GDALINST/gdal-$GDALVERSION $GDALOPTS
-  make -s -j 2
+  make -j 2
   make install
 fi
 

tests/conftest.py
@@ -14,7 +14,7 @@ def pytest_report_header(config):
     headers = []
     # gdal version number
     gdal_release_name = fiona.get_gdal_release_name().decode("utf-8")
-    headers.append('GDAL: {}'.format(gdal_release_name))
+    headers.append('GDAL: {} ({})'.format(gdal_release_name, fiona.get_gdal_version_num()))
     supported_drivers = ", ".join(sorted(list(fiona.supported_drivers.keys())))
     # supported drivers
     headers.append("Supported drivers: {}".format(supported_drivers))

tests/test_bytescollection.py
@@ -9,8 +9,6 @@ import six
 import fiona
 
 @pytest.mark.usefixtures('uttc_path_coutwildrnp_json')
-
-
 class ReadingTest(unittest.TestCase):
 
     def setUp(self):
@@ -221,7 +219,9 @@ def test_zipped_bytes_collection(bytes_coutwildrnp_zip):
         assert col.name == 'coutwildrnp'
         assert len(col) == 67
 
-
+@pytest.mark.skipif(fiona.get_gdal_version_num() >= fiona.calc_gdal_version_num(2, 3, 0),
+    reason="Changed behavior with gdal 2.3, possibly related to RFC 70:"
+    "Guessing output format from output file name extension for utilities")
 def test_grenada_bytes_geojson(bytes_grenada_geojson):
     """Read grenada.geojson as BytesCollection.
 

tests/test_collection.py
@@ -922,13 +922,13 @@ class OpenKeywordArgsTest(unittest.TestCase):
 
 @pytest.mark.network
 def test_collection_http():
-    ds = fiona.Collection('http://svn.osgeo.org/gdal/trunk/autotest/ogr/data/poly.shp', vsi='http')
-    assert ds.path == '/vsicurl/http://svn.osgeo.org/gdal/trunk/autotest/ogr/data/poly.shp'
+    ds = fiona.Collection('http://raw.githubusercontent.com/OSGeo/gdal/master/autotest/ogr/data/poly.shp', vsi='http')
+    assert ds.path == '/vsicurl/http://raw.githubusercontent.com/OSGeo/gdal/master/autotest/ogr/data/poly.shp'
     assert len(ds) == 10
 
 @pytest.mark.network
 def test_collection_zip_http():
-    ds = fiona.Collection('http://svn.osgeo.org/gdal/trunk/autotest/ogr/data/poly.zip', vsi='zip+http')
-    assert ds.path == '/vsizip/vsicurl/http://svn.osgeo.org/gdal/trunk/autotest/ogr/data/poly.zip'
+    ds = fiona.Collection('http://raw.githubusercontent.com/OSGeo/gdal/master/autotest/ogr/data/poly.zip', vsi='zip+http')
+    assert ds.path == '/vsizip/vsicurl/http://raw.githubusercontent.com/OSGeo/gdal/master/autotest/ogr/data/poly.zip'
     assert len(ds) == 10
         

tests/test_vfs.py
@@ -162,7 +162,7 @@ class TarArchiveReadingTest(VsiReadingTest):
 
 @pytest.mark.network        
 def test_open_http():
-    ds = fiona.open('http://svn.osgeo.org/gdal/trunk/autotest/ogr/data/poly.shp')
+    ds = fiona.open('https://raw.githubusercontent.com/OSGeo/gdal/master/autotest/ogr/data/poly.shp')
     assert len(ds) == 10
 
 @credentials

commit 0a4071351cd755700e79467dda3ef60175548eee

Fix s3 regression introduced in f70b44c
Plus a few comments and code tidying.

fiona/vfs.py
@@ -5,10 +5,24 @@ import sys
 import re
 from fiona.compat import urlparse
 
-# Supported URI schemes and their mapping to GDAL's VSI suffix. 
-SCHEMES = {'gzip': 'gzip', 'zip': 'zip', 'tar': 'tar', 'https': 'curl',
-           'http': 'curl', 's3': 's3'}
-REMOTESCHEMES = set(('https', 'http', 's3',))
+
+# Supported URI schemes and their mapping to GDAL's VSI suffix.
+# TODO: extend for other cloud plaforms.
+SCHEMES = {
+    'ftp': 'curl',
+    'gzip': 'gzip',
+    'http': 'curl',
+    'https': 'curl',
+    's3': 's3',
+    'tar': 'tar',
+    'zip': 'zip',
+}
+
+CURLSCHEMES = set([k for k, v in SCHEMES.items() if v == 'curl'])
+
+# TODO: extend for other cloud plaforms.
+REMOTESCHEMES = set([k for k, v in SCHEMES.items() if v in ('curl', 's3')])
+
 
 def valid_vsi(vsi):
     """Ensures all parts of our vsi path are valid schemes."""
@@ -19,8 +33,9 @@ def is_remote(scheme):
         return False
     return any(p in REMOTESCHEMES for p in scheme.split('+'))
 
+
 def vsi_path(path, vsi=None, archive=None):
-    # If a VSF and archive file are specified, we convert the path to
+    # If a VSI and archive file are specified, we convert the path to
     # an OGR VSI path (see cpl_vsi.h).
     if vsi:
         prefix = '/'.join('vsi{0}'.format(SCHEMES[p]) for p in vsi.split('+'))
@@ -57,7 +72,7 @@ def parse_paths(uri, vfs=None):
         scheme = parts.scheme
         path = parts.path
         if parts.netloc and parts.netloc != 'localhost':
-            if scheme.split("+")[-1] in REMOTESCHEMES:
+            if scheme.split("+")[-1] in CURLSCHEMES:
                 # We need to deal with cases such as zip+https://server.com/data.zip
                 path = "{}://{}{}".format(scheme.split("+")[-1], parts.netloc, path)
             else: