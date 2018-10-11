2421212be1072db1428e7c832c0818a3928c1153
Sean C. Gillies <sean@mapbox.com>
2018-10-11 09:47:14 -0600

    Add test for collection creation with crs_wkt

--- tests/test_collection_crs.py
+++ tests/test_collection_crs.py
@@ -6,6 +6,7 @@ import fiona.crs
 
 from .conftest import WGS84PATTERN
 
+
 def test_collection_crs_wkt(path_coutwildrnp_shp):
     with fiona.open(path_coutwildrnp_shp) as src:
         assert re.match(WGS84PATTERN, src.crs_wkt)
@@ -21,3 +22,14 @@ def test_collection_no_crs_wkt(tmpdir, path_coutwildrnp_shp):
     with fiona.open(filename, 'w', **profile) as dst:
         assert dst.crs_wkt == ""
         assert dst.crs == {}
+
+
+def test_collection_create_crs_wkt(tmpdir):
+    """A collection can be created using crs_wkt"""
+    filename = str(tmpdir.join("test.shp"))
+    wkt = 'GEOGCS["GCS_WGS_1984",DATUM["WGS_1984",SPHEROID["WGS_84",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295],AUTHORITY["EPSG","4326"]]'
+    with fiona.open(filename, 'w', schema={'geometry': 'Point', 'properties': {'foo': 'int'}}, crs_wkt=wkt, driver='GeoJSON') as dst:
+        assert dst.crs_wkt == wkt
+
+    with fiona.open(filename) as col:
+        assert col.crs_wkt.startswith('GEOGCS["WGS 84')
