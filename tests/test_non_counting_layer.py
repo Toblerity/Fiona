773aee3fad3ef5e0924f04bebe6536510d6148b0
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2018-10-20 04:13:04 -0400

    Remove unittest from test_non_counting_layer.py.

--- tests/test_non_counting_layer.py
+++ tests/test_non_counting_layer.py
@@ -1,5 +1,3 @@
-import unittest
-
 import pytest
 
 import fiona
@@ -7,34 +5,34 @@ from fiona.errors import FionaDeprecationWarning
 
 
 @pytest.mark.usefixtures('uttc_path_gpx')
-class NonCountingLayerTest(unittest.TestCase):
-    def setUp(self):
+class TestNonCountingLayer(object):
+    def setup(self):
         self.c = fiona.open(self.path_gpx, "r", layer="track_points")
 
-    def tearDown(self):
+    def teardown(self):
         self.c.close()
 
     def test_len_fail(self):
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             len(self.c)
 
     def test_list(self):
         features = list(self.c)
-        self.assertEqual(len(features), 19)
+        assert len(features) == 19
 
     def test_getitem(self):
         self.c[2]
 
     def test_fail_getitem_negative_index(self):
-        with self.assertRaises(IndexError):
+        with pytest.raises(IndexError):
             self.c[-1]
 
     def test_slice(self):
         with pytest.warns(FionaDeprecationWarning):
             features = self.c[2:5]
-            self.assertEqual(len(features), 3)
+            assert len(features) == 3
 
     def test_fail_slice_negative_index(self):
         with pytest.warns(FionaDeprecationWarning):
-            with self.assertRaises(IndexError):
+            with pytest.raises(IndexError):
                 self.c[2:-4]
