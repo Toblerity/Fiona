52c674aa04badd86c424726aa3cf2eaec252ae5d
Sean Gillies <sean@mapbox.com>
2018-10-23 08:17:45 -0600

    import Mapping from collections.abc when possible

--- fiona/compat.py
+++ fiona/compat.py
@@ -15,7 +15,12 @@ else:
     from UserDict import UserDict
     from inspect import getargspec
 
+if sys.version_info >= (3, 3):
+    from collections.abc import Mapping
+else:
+    from collections import Mapping
+
 # Users can pass in objects that subclass a few different objects
 # More specifically, rasterio has a CRS() class that subclasses UserDict()
 # In Python 2 UserDict() is in its own module and does not subclass Mapping()
-DICT_TYPES = (dict, collections.Mapping, UserDict)
+DICT_TYPES = (dict, Mapping, UserDict)
