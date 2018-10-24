4e5c61009b2417630d1137b680691df001544d1e
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2018-10-24 18:06:12 -0400

    Drop unittest in multiconnection tests (#654)
    * Convert multi-connection read tests to pytest.
    
    * Replace assertEqual in multi-connection tests.
    
    * Convert multi-connection read/write tests to pytest.
    
    * Convert multi-connection layer creation tests to pytest.

--- tests/test_multiconxn.py
+++ tests/test_multiconxn.py
@@ -1,47 +1,38 @@
-import logging
-import os
-import shutil
-import sys
-import tempfile
-import unittest
-
 import pytest
 
 import fiona
 from fiona.compat import OrderedDict
 
 
-class ReadAccess(unittest.TestCase):
+class TestReadAccess(object):
     # To check that we'll be able to get multiple 'r' connections to layers
     # in a single file.
 
-    def setUp(self):
-        self.c = fiona.open("tests/data/coutwildrnp.shp", "r", layer="coutwildrnp")
+    def test_meta(self, path_coutwildrnp_shp):
+        with fiona.open(path_coutwildrnp_shp, "r", layer="coutwildrnp") as c:
+            with fiona.open(path_coutwildrnp_shp, "r",
+                            layer="coutwildrnp") as c2:
+                assert len(c) == len(c2)
+                assert sorted(c.schema.items()) == sorted(c2.schema.items())
 
-    def tearDown(self):
-        self.c.close()
-
-    def test_meta(self):
-        with fiona.open("tests/data/coutwildrnp.shp", "r", layer="coutwildrnp") as c2:
-            self.assertEqual(len(self.c), len(c2))
-            self.assertEqual(sorted(self.c.schema.items()), sorted(c2.schema.items()))
-
-    def test_feat(self):
-        f1 = next(iter(self.c))
-        with fiona.open("tests/data/coutwildrnp.shp", "r", layer="coutwildrnp") as c2:
-            f2 = next(iter(c2))
-            self.assertEqual(f1, f2)
+    def test_feat(self, path_coutwildrnp_shp):
+        with fiona.open(path_coutwildrnp_shp, "r", layer="coutwildrnp") as c:
+            f1 = next(iter(c))
+            with fiona.open(path_coutwildrnp_shp, "r",
+                            layer="coutwildrnp") as c2:
+                f2 = next(iter(c2))
+                assert f1 == f2
 
 
-class ReadWriteAccess(unittest.TestCase):
+class TestReadWriteAccess(object):
     # To check that we'll be able to read from a file that we're
     # writing to.
 
-    def setUp(self):
-        self.tempdir = tempfile.mkdtemp()
+    @pytest.fixture(autouse=True)
+    def multi_write_test_shp(self, tmpdir):
+        self.shapefile_path = str(tmpdir.join("multi_write_test.shp"))
         self.c = fiona.open(
-            os.path.join(self.tempdir, "multi_write_test.shp"),
-            "w",
+            self.shapefile_path, "w",
             driver="ESRI Shapefile",
             schema={
                 'geometry': 'Point',
@@ -54,43 +45,37 @@ class ReadWriteAccess(unittest.TestCase):
             'properties': OrderedDict([('title', 'point one'), ('date', '2012-01-29')])}
         self.c.writerecords([self.f])
         self.c.flush()
-
-    def tearDown(self):
+        yield
         self.c.close()
-        shutil.rmtree(self.tempdir)
 
     def test_meta(self):
-        c2 = fiona.open(os.path.join(self.tempdir, "multi_write_test.shp"), "r")
-        self.assertEqual(len(self.c), len(c2))
-        self.assertEqual(sorted(self.c.schema.items()), sorted(c2.schema.items()))
+        c2 = fiona.open(self.shapefile_path, "r")
+        assert len(self.c) == len(c2)
+        assert sorted(self.c.schema.items()) == sorted(c2.schema.items())
         c2.close()
 
     def test_read(self):
-        c2 = fiona.open(os.path.join(self.tempdir, "multi_write_test.shp"), "r")
+        c2 = fiona.open(self.shapefile_path, "r")
         f2 = next(iter(c2))
         del f2['id']
-        self.assertEqual(self.f, f2)
+        assert self.f == f2
         c2.close()
 
     def test_read_after_close(self):
-        c2 = fiona.open(os.path.join(self.tempdir, "multi_write_test.shp"), "r")
+        c2 = fiona.open(self.shapefile_path, "r")
         self.c.close()
         f2 = next(iter(c2))
         del f2['id']
-        self.assertEqual(self.f, f2)
+        assert self.f == f2
         c2.close()
 
-class LayerCreation(unittest.TestCase):
 
-    def setUp(self):
-        self.tempdir = tempfile.mkdtemp()
-        self.dir = os.path.join(self.tempdir, 'layer_creation')
-        if os.path.exists(self.dir):
-            shutil.rmtree(self.dir)
-        os.mkdir(self.dir)
+class TestLayerCreation(object):
+    @pytest.fixture(autouse=True)
+    def layer_creation_shp(self, tmpdir):
+        self.dir = tmpdir.mkdir('layer_creation')
         self.c = fiona.open(
-            self.dir,
-            'w',
+            str(self.dir), 'w',
             layer='write_test',
             driver='ESRI Shapefile',
             schema={
@@ -104,28 +89,26 @@ class LayerCreation(unittest.TestCase):
             'properties': OrderedDict([('title', 'point one'), ('date', '2012-01-29')])}
         self.c.writerecords([self.f])
         self.c.flush()
-
-    def tearDown(self):
+        yield
         self.c.close()
-        shutil.rmtree(self.tempdir)
 
     def test_meta(self):
-        c2 = fiona.open(os.path.join(self.dir, "write_test.shp"), "r")
-        self.assertEqual(len(self.c), len(c2))
-        self.assertEqual(sorted(self.c.schema.items()), sorted(c2.schema.items()))
+        c2 = fiona.open(str(self.dir.join("write_test.shp")), "r")
+        assert len(self.c) == len(c2)
+        assert sorted(self.c.schema.items()) == sorted(c2.schema.items())
         c2.close()
 
     def test_read(self):
-        c2 = fiona.open(os.path.join(self.dir, "write_test.shp"), "r")
+        c2 = fiona.open(str(self.dir.join("write_test.shp")), "r")
         f2 = next(iter(c2))
         del f2['id']
-        self.assertEqual(self.f, f2)
+        assert self.f == f2
         c2.close()
 
     def test_read_after_close(self):
-        c2 = fiona.open(os.path.join(self.dir, "write_test.shp"), "r")
+        c2 = fiona.open(str(self.dir.join("write_test.shp")), "r")
         self.c.close()
         f2 = next(iter(c2))
         del f2['id']
-        self.assertEqual(self.f, f2)
+        assert self.f == f2
         c2.close()
