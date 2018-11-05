c40c913233cd4b4173b05fed77d84e69d39e3255
Sean Gillies <sean@mapbox.com>
2018-11-05 10:09:43 -0700

    Eliminate log messages about ENCODING
    Resolves #668

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -866,3 +866,10 @@ def test_collection_zip_http():
     ds = fiona.Collection('http://raw.githubusercontent.com/OSGeo/gdal/master/autotest/ogr/data/poly.zip', vsi='zip+http')
     assert ds.path == '/vsizip/vsicurl/http://raw.githubusercontent.com/OSGeo/gdal/master/autotest/ogr/data/poly.zip'
     assert len(ds) == 10
+
+
+def test_encoding_option_warning(tmpdir, caplog):
+    """There is no ENCODING creation option log warning for GeoJSON"""
+    ds = fiona.Collection(str(tmpdir.join("test.geojson")), "w", driver="GeoJSON", crs="epsg:4326",
+            schema={"geometry": "Point", "properties": {"foo": "int"}})
+    assert not caplog.text
