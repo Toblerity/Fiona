cae65dae3b6fd241a755180e2116c9bdcbacfad5
Sean Gillies <sean.gillies@gmail.com>
2019-06-11 07:55:13 -0600

    Reformat, give up on dict instance checks

--- fiona/model.py
+++ fiona/model.py
@@ -7,9 +7,19 @@ from fiona.errors import FionaDeprecationWarning
 
 
 class Object(MutableMapping):
+    """Base class for CRS, geometry, and feature objects
+
+    In Fiona 2.0, the implementation of those objects will change.  They
+    will no longer be dicts or derive from dict, and will lose some
+    features like mutability and default JSON serialization.
+
+    Object will be used for these objects in Fiona 1.9. This class warns
+    about future deprecation of features.
+    """
 
     def __init__(self, **data):
-        self._data = data.copy()
+        self._data = {}
+        self._data.update(data)
 
     def __getitem__(self, item):
         return self._data[item]
@@ -21,9 +31,17 @@ class Object(MutableMapping):
         return len(self._data)
 
     def __setitem__(self, key, value):
-        warn("Object will become immutable in version 2.0", FionaDeprecationWarning, stacklevel=2)
+        warn(
+            "instances of this class -- CRS, geometry, and feature objects -- will become immutable in fiona version 2.0",
+            FionaDeprecationWarning,
+            stacklevel=2,
+        )
         self._data[key] = value
 
     def __delitem__(self, key):
-        warn("Object will become immutable in version 2.0", FionaDeprecationWarning, stacklevel=2)
+        warn(
+            "instances of this class -- CRS, geometry, and feature objects -- will become immutable in fiona version 2.0",
+            FionaDeprecationWarning,
+            stacklevel=2,
+        )
         del self._data[key]
