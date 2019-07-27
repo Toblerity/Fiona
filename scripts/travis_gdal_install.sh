963bdd5579f7dc06e96eab44d690ef49658c5264
René Buffat <buffat@gmail.com>
2019-07-28 00:16:38 +0200

    sudo dpkg

--- scripts/travis_gdal_install.sh
+++ scripts/travis_gdal_install.sh
@@ -68,9 +68,8 @@ if [ "$GDALVERSION" = "trunk" ]; then
 
 elif ( curl -o/dev/null -sfI "https://rbuffat.github.io/gdal_builder/gdal_$GDALVERSION-1_amd64.deb" ); then
   # install deb when available
-  
   wget https://rbuffat.github.io/gdal_builder/gdal_$GDALVERSION-1_amd64.deb
-  dpkg -i gdal_$GDALVERSION-1_amd64.deb
+  sudo dpkg -i gdal_$GDALVERSION-1_amd64.deb
   rm gdal_$GDALVERSION-1_amd64.deb
 
 elif [ ! -d "$GDALINST/gdal-$GDALVERSION" ]; then
