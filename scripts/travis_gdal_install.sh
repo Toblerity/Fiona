3386560ee0a1211c400b7f882a87d917866b7fee
Sean Gillies <sean@mapbox.com>
2019-10-17 15:45:23 -0600

    Update GDAL build script

--- scripts/travis_gdal_install.sh
+++ scripts/travis_gdal_install.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+#
+# originally contributed by @rbuffat to Toblerity/Fiona
 set -e
 
 GDALOPTS="  --with-ogr \
             --with-geos \
             --with-expat \
             --without-libtool \
+            --with-libz=internal \
             --with-libtiff=internal \
             --with-geotiff=internal \
             --without-gif \
@@ -13,7 +16,7 @@ GDALOPTS="  --with-ogr \
             --without-libgrass \
             --without-cfitsio \
             --without-pcraster \
-            --without-netcdf \
+            --with-netcdf \
             --with-png=internal \
             --with-jpeg=internal \
             --without-gif \
@@ -34,12 +37,10 @@ GDALOPTS="  --with-ogr \
             --without-odbc \
             --with-curl \
             --with-sqlite3 \
-            --without-dwgdirect \
             --without-idb \
             --without-sde \
-            --without-perl \
-            --without-php \
             --without-ruby \
+            --without-perl \
             --without-python"
 
 # Create build dir if not exists
@@ -53,22 +54,62 @@ fi
 
 ls -l $GDALINST
 
-if [ "$GDALVERSION" = "trunk" ]; then
-  # always rebuild trunk
-  git clone -b master --single-branch --depth=1 https://github.com/OSGeo/gdal.git $GDALBUILD/trunk
-  cd $GDALBUILD/trunk/gdal
-  ./configure --prefix=$GDALINST/gdal-$GDALVERSION $GDALOPTS
-  make -j 2
-  make install
-elif [ ! -d "$GDALINST/gdal-$GDALVERSION" ]; then
-  # only build if not already installed
-  cd $GDALBUILD
-  wget http://download.osgeo.org/gdal/$GDALVERSION/gdal-$GDALVERSION.tar.gz
-  tar -xzf gdal-$GDALVERSION.tar.gz
-  cd gdal-$GDALVERSION
-  ./configure --prefix=$GDALINST/gdal-$GDALVERSION $GDALOPTS
-  make -j 2
-  make install
+if [ "$GDALVERSION" = "master" ]; then
+    PROJOPT="--with-proj=$GDALINST/gdal-$GDALVERSION"
+    cd $GDALBUILD
+    git clone --depth 1 https://github.com/OSGeo/gdal gdal-$GDALVERSION
+    cd gdal-$GDALVERSION/gdal
+    git rev-parse HEAD > newrev.txt
+    BUILD=no
+    # Only build if nothing cached or if the GDAL revision changed
+    if test ! -f $GDALINST/gdal-$GDALVERSION/rev.txt; then
+        BUILD=yes
+    elif ! diff newrev.txt $GDALINST/gdal-$GDALVERSION/rev.txt >/dev/null; then
+        BUILD=yes
+    fi
+    if test "$BUILD" = "yes"; then
+        mkdir -p $GDALINST/gdal-$GDALVERSION
+        cp newrev.txt $GDALINST/gdal-$GDALVERSION/rev.txt
+        ./configure --prefix=$GDALINST/gdal-$GDALVERSION $GDALOPTS $PROJOPT
+        make -s -j 2
+        make install
+    fi
+
+else
+    case "$GDALVERSION" in
+        3*)
+            PROJOPT="--with-proj=$GDALINST/gdal-$GDALVERSION"
+            ;;
+        2.4*)
+            PROJOPT="--with-proj=$GDALINST/gdal-$GDALVERSION"
+            ;;
+        2.3*)
+            PROJOPT="--with-proj=$GDALINST/gdal-$GDALVERSION"
+            ;;
+        2.2*)
+            PROJOPT="--with-static-proj4=$GDALINST/gdal-$GDALVERSION"
+            ;;
+        2.1*)
+            PROJOPT="--with-static-proj4=$GDALINST/gdal-$GDALVERSION"
+            ;;
+        2.0*)
+            PROJOPT="--with-static-proj4=$GDALINST/gdal-$GDALVERSION"
+            ;;
+        1*)
+            PROJOPT="--with-static-proj4=$GDALINST/gdal-$GDALVERSION"
+            ;;
+    esac
+
+    if [ ! -d "$GDALINST/gdal-$GDALVERSION/share/gdal" ]; then
+        cd $GDALBUILD
+        gdalver=$(expr "$GDALVERSION" : '\([0-9]*.[0-9]*.[0-9]*\)')
+        wget -q http://download.osgeo.org/gdal/$gdalver/gdal-$GDALVERSION.tar.gz
+        tar -xzf gdal-$GDALVERSION.tar.gz
+        cd gdal-$gdalver
+        ./configure --prefix=$GDALINST/gdal-$GDALVERSION $GDALOPTS $PROJOPT
+        make -s -j 2
+        make install
+    fi
 fi
 
 # change back to travis build dir
