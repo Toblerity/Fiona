c45376d129b9a84e64149fbe00bb33d7e35040ba
Mike Taves <mwtoews@gmail.com>
2019-11-06 11:47:38 +1300

    Drop Python 2.7 and 3.5, add Python 3.8 and upgrade GDAL releases

--- setup.py
+++ setup.py
@@ -334,12 +334,10 @@ setup_args = dict(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering :: GIS'])
 
 if os.environ.get('PACKAGE_DATA'):
