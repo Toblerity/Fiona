9d011b50afd73d97cf2277e14e0753af679c1450
Sean Gillies <sean@mapbox.com>
2019-11-05 13:27:11 -0700

    Remove derelict _drivers module
    Update change log and version

--- fiona/__init__.py
+++ fiona/__init__.py
@@ -101,7 +101,7 @@ import uuid
 
 
 __all__ = ['bounds', 'listlayers', 'open', 'prop_type', 'prop_width']
-__version__ = "1.8.9.post2"
+__version__ = "1.8.10dev"
 __gdal_version__ = get_gdal_release_name()
 
 gdal_version = get_gdal_version_tuple()
