2c1cef667160e226f6ee5af1cbb0815d1295d5bf
Sean Gillies <sean@mapbox.com>
2019-12-05 07:21:39 -0700

    Note fix, bump version

--- fiona/__init__.py
+++ fiona/__init__.py
@@ -102,7 +102,7 @@ import uuid
 
 
 __all__ = ['bounds', 'listlayers', 'open', 'prop_type', 'prop_width']
-__version__ = "1.8.12"
+__version__ = "1.8.13"
 __gdal_version__ = get_gdal_release_name()
 
 gdal_version = get_gdal_version_tuple()
