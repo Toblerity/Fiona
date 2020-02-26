2c30e31a80713287815a677679488d6244bd3720
René Buffat <buffat@gmail.com>
2020-02-26 10:53:57 +0100

    add tests for bounds after writing

--- tests/test_bounds.py
+++ tests/test_bounds.py
@@ -1,5 +1,7 @@
 import fiona
-
+import pytest
+from fiona.drvsupport import supported_drivers
+from .conftest import driver_extensions
 
 def test_bounds_point():
     g = {'type': 'Point', 'coordinates': [10, 10]}
@@ -19,3 +21,30 @@ def test_bounds_polygon():
 def test_bounds_z():
     g = {'type': 'Point', 'coordinates': [10,10,10]}
     assert fiona.bounds(g) == (10, 10, 10, 10)
+
+
+blacklist_write_drivers = set(['CSV', 'GPX', 'GPSTrackMaker', 'DXF', 'DGN'])
+write_drivers = [driver for driver, raw in supported_drivers.items() if 'w' in raw and driver not in blacklist_write_drivers]
+@pytest.mark.parametrize('driver', write_drivers)
+def test_bounds(tmpdir, driver):
+    """Test if bounds are correctly calculated after writing
+    
+    """
+    extension = driver_extensions.get(driver, "bar")
+    path = str(tmpdir.join('foo.{}'.format(extension)))
+
+    with fiona.open(path, 'w',
+                    driver=driver,
+                    schema={'geometry': 'Point',
+                            'properties': [('title', 'str')]},
+                    fiona_force_driver=True) as c:
+
+        c.writerecords([{'geometry': {'type': 'Point', 'coordinates': (1.0, 10.0)},
+                            'properties': {'title': 'One'}}])
+
+        assert c.bounds == (1.0, 10.0, 1.0, 10.0)
+
+        c.writerecords([{'geometry': {'type': 'Point', 'coordinates': (2.0, 20.0)},
+                            'properties': {'title': 'One'}}])
+        
+        assert c.bounds == (1.0, 10.0, 2.0, 20.0)
