a06668f8419db48bd615663e520bed05dc5c59c2
René Buffat <buffat@gmail.com>
2020-04-19 23:29:35 +0200

    enable coverage for cython pyx files on travis

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 import shutil
 import subprocess
 import sys
-
+import glob
 from setuptools import setup
 from setuptools.extension import Extension
 
@@ -190,6 +190,16 @@ ext_options = dict(
     library_dirs=library_dirs,
     libraries=libraries,
     extra_link_args=extra_link_args)
+
+# Enable coverage for cython pyx files.
+if os.getenv("CYTHON_TRACING", "False").lower() == "true":
+    from Cython.Compiler.Options import get_directive_defaults
+    directive_defaults = get_directive_defaults()
+    directive_defaults['linetrace'] = True
+    directive_defaults['binding'] = True
+
+    ext_options.update(dict(
+        define_macros=[("CYTHON_TRACE_NOGIL", "1")]))
 
 # GDAL 2.3+ requires C++11
 
