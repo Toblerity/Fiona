37da85db8858a67a8a06451013af9aafd956ea33
Sean Gillies <sean@mapbox.com>
2021-04-07 13:41:03 -0600

    Bring env.py and _env.pyx current with rasterio

--- fiona/env.py
+++ fiona/env.py
@@ -597,36 +597,41 @@ def require_gdal_version(version, param=None, values=None, is_max_version=False,
 
 # Patch the environment if needed, such as in the installed wheel case.
 
-if "GDAL_DATA" not in os.environ:
+if 'GDAL_DATA' not in os.environ:
 
     path = GDALDataFinder().search_wheel()
 
     if path:
-        log.debug("GDAL data files found in package: path=%r", path)
+        log.debug("GDAL data found in package: path=%r.", path)
         set_gdal_config("GDAL_DATA", path)
 
     # See https://github.com/mapbox/rasterio/issues/1631.
     elif GDALDataFinder().find_file("header.dxf"):
-        log.debug("GDAL data files found at built-in paths")
+        log.debug("GDAL data files are available at built-in paths.")
 
     else:
         path = GDALDataFinder().search()
 
         if path:
-            log.debug("GDAL data files found: path=%r", path)
             set_gdal_config("GDAL_DATA", path)
+            log.debug("GDAL data found in other locations: path=%r.", path)
 
 if "PROJ_LIB" in os.environ:
     path = os.environ["PROJ_LIB"]
     set_proj_data_search_path(path)
 
+elif PROJDataFinder().search_wheel():
+    path = PROJDataFinder().search_wheel()
+    log.debug("PROJ data found in package: path=%r.", path)
+    set_proj_data_search_path(path)
+
 # See https://github.com/mapbox/rasterio/issues/1631.
 elif PROJDataFinder().has_data():
-    log.debug("PROJ data files are available at built-in paths")
+    log.debug("PROJ data files are available at built-in paths.")
 
 else:
     path = PROJDataFinder().search()
 
     if path:
-        log.debug("PROJ data found: path=%r", path)
+        log.debug("PROJ data found in other locations: path=%r.", path)
         set_proj_data_search_path(path)
