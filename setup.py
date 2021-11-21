95dabf7fb8b3d72980d37af119d509d6e266445d
Mike Taves <mwtoews@gmail.com>
2021-11-22 09:11:13 +1300

    Add support for Python 3.10, GDAL 3.3.0

--- setup.py
+++ setup.py
@@ -340,6 +340,8 @@ setup_args = dict(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Scientific/Engineering :: GIS'])
 
 if os.environ.get('PACKAGE_DATA'):
