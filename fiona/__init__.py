00f7c136bacfd7e6825876dd9ac585120b95dcb0
Sean Gillies <sean.gillies@gmail.com>
2022-02-07 08:04:16 -0700

    Finalize version and set date in change log

--- fiona/__init__.py
+++ fiona/__init__.py
@@ -105,7 +105,7 @@ with fiona._loading.add_gdal_dll_directories():
 
 
 __all__ = ['bounds', 'listlayers', 'open', 'prop_type', 'prop_width']
-__version__ = "1.8.21dev"
+__version__ = "1.8.21"
 __gdal_version__ = get_gdal_release_name()
 
 gdal_version = get_gdal_version_tuple()
