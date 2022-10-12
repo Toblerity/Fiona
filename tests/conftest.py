9e1a5ea453ff07553770607ac05b9de1024cfa4d
René Buffat <buffat@gmail.com>
2022-10-12 20:27:15 +0200

    use -m gdal instead of --test_gdal

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,22 +16,14 @@ from fiona.meta import extensions
 from fiona.model import Feature, ObjectEncoder, to_dict
 
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--test_gdal", action="store_true", default=False, help="run tests only depended on gdal functionality"
-    )
-
-
 def pytest_collection_modifyitems(config, items):
 
-    if config.getoption("--test_gdal"):
-        return
-
-    if not config.getoption('-m'):
-        skip_gdal = pytest.mark.skip(reason="need --test_gdal option to run")
+    if not config.getoption("-m"):
+        skip_gdal = pytest.mark.skip(reason="use '-m gdal' to run GDAL related tests.")
         for item in items:
             if "gdal" in item.keywords:
                 item.add_marker(skip_gdal)
+
 
 def pytest_report_header(config):
     headers = []
