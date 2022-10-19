f83c7d073ce3664d2649651edfb6bf518fd42aa4
René Buffat <buffat@gmail.com>
2022-10-19 21:21:46 +0200

    skip @pytest.mark.gdal marked tests per default

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,10 +18,19 @@ from fiona.model import Feature, ObjectEncoder, to_dict
 
 def pytest_collection_modifyitems(config, items):
 
-    if not config.getoption("-m"):
+    # Fiona contains some tests that depend only on GDALs behavior.
+    # E.g. some test the driver specific access modes maintained in
+    # fiona/drvsupport.py for different GDAL versions.
+    # These tests can fail on exotic architectures (e.g. not returning 
+    # the exact same value)
+    # We explicitly enable these tests on Fiona CI using pytest -m gdal
+    # and hide these tests otherwise.
+    markers_options = config.getoption("-m", "")
+    if "gdal" not in markers_options:
         skip_gdal = pytest.mark.skip(reason="use '-m gdal' to run GDAL related tests.")
         for item in items:
-            if "gdal" in item.keywords:
+            gdal_marker = item.get_closest_marker("gdal")
+            if gdal_marker is not None and gdal_marker.name == "gdal":
                 item.add_marker(skip_gdal)
 
 
