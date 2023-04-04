1d02089c39b67956894e029b0a866090b4f63abd
Sean Gillies <sean.gillies@gmail.com>
2023-04-04 11:07:43 -0600

    Use RTD theme, begin content on home page
    Resolves #1241

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,14 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.todo',
+    'sphinx_click',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -97,7 +104,8 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'default'
-html_theme = 'sphinxdoc'
+#html_theme = 'sphinxdoc'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,12 @@
-Fiona Documentation Contents
-============================
+===============================================
+Fiona: access to simple geospatial feature data
+===============================================
+
+Fiona streams simple feature data to and from GIS formats like GeoPackage and
+Shapefile. Fiona can read and write real-world data using multi-layered GIS
+formats, zipped and in-memory virtual file systems, from files on your hard
+drive or in cloud storage. This project includes Python modules and a command
+line interface (CLI).
 
 .. toctree::
    :maxdepth: 2
