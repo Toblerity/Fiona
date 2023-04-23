293bc388bc1ba0aef1f1d08c706331973b351a05
Sean Gillies <sean.gillies@gmail.com>
2023-04-23 17:27:58 -0600

    Rewrite module intro

--- fiona/__init__.py
+++ fiona/__init__.py
@@ -6,58 +6,18 @@ source GIS community's most trusted geodata access library and
 integrates readily with other Python GIS packages such as pyproj, Rtree
 and Shapely.
 
-How minimal? Fiona can read features as mappings from shapefiles or
-other GIS vector formats and write mappings as features to files using
-the same formats. That's all. There aren't any feature or geometry
-classes. Features and their geometries are just data.
-
 A Fiona feature is a Python mapping inspired by the GeoJSON format. It
-has `id`, 'geometry`, and `properties` keys. The value of `id` is
-a string identifier unique within the feature's parent collection. The
-`geometry` is another mapping with `type` and `coordinates` keys. The
-`properties` of a feature is another mapping corresponding to its
-attribute table. For example:
+has ``id``, ``geometry``, and ``properties`` attributes. The value of
+``id`` is a string identifier unique within the feature's parent
+collection. The ``geometry`` is another mapping with ``type`` and
+``coordinates`` keys. The ``properties`` of a feature is another mapping
+corresponding to its attribute table.
 
-  {'id': '1',
-   'geometry': {'type': 'Point', 'coordinates': (0.0, 0.0)},
-   'properties': {'label': 'Null Island'} }
+Features are read and written using the ``Collection`` class.  These
+``Collection`` objects are a lot like Python ``file`` objects. A
+``Collection`` opened in reading mode serves as an iterator over
+features. One opened in a writing mode provides a ``write`` method.
 
-is a Fiona feature with a point geometry and one property.
-
-Features are read and written using objects returned by the
-``collection`` function. These ``Collection`` objects are a lot like
-Python ``file`` objects. A ``Collection`` opened in reading mode serves
-as an iterator over features. One opened in a writing mode provides
-a ``write`` method.
-
-Usage
------
-
-Here's an example of reading a select few polygon features from
-a shapefile and for each, picking off the first vertex of the exterior
-ring of the polygon and using that as the point geometry for a new
-feature writing to a "points.shp" file.
-
-  >>> import fiona
-  >>> with fiona.open('docs/data/test_uk.shp', 'r') as inp:
-  ...     output_schema = inp.schema.copy()
-  ...     output_schema['geometry'] = 'Point'
-  ...     with collection(
-  ...             "points.shp", "w",
-  ...             crs=inp.crs,
-  ...             driver="ESRI Shapefile",
-  ...             schema=output_schema
-  ...             ) as out:
-  ...         for f in inp.filter(
-  ...                 bbox=(-5.0, 55.0, 0.0, 60.0)
-  ...                 ):
-  ...             value = f['geometry']['coordinates'][0][0]
-  ...             f['geometry'] = {
-  ...                 'type': 'Point', 'coordinates': value}
-  ...             out.write(f)
-
-Because Fiona collections are context managers, they are closed and (in
-writing modes) flush contents to disk when their ``with`` blocks end.
 """
 
 import glob
