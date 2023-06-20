b26afaaa9388fc5e4a361097caac32152f35a7f6
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2023-06-20 15:21:17 +0200

    Extracting additional method interface from main.py

--- fiona/fio/main.pyi
+++ fiona/fio/main.pyi
@@ -1,1 +1,11 @@
 def configure_logging(verbosity: int) -> None: ...
+
+
+def main_group(
+    ctx,
+    verbose: bool,
+    quiet: bool,
+    aws_profile: str,
+    aws_no_sign_requests: bool,
+    aws_requester_pays: bool,
+) -> None: ...
