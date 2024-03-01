7b6cd5cfd9cfab11fbbbe4a27e0a4549d5067048
Sean Gillies <sean.gillies@tiledb.com>
2024-03-01 08:54:04 -0700

    Assert that drive letter remains and ! is gone in win32 path test

--- tests/test__path.py
+++ tests/test__path.py
@@ -11,4 +11,5 @@ def test_parse_zip_windows(monkeypatch):
     monkeypatch.setattr(sys, "platform", "win32")
     path = _parse_path("zip://D:\\a\\Fiona\\Fiona\\tests\\data\\coutwildrnp.zip!coutwildrnp.shp")
     vsi_path = _vsi_path(path)
-    assert Path(vsi_path).as_posix() == "/vsizip/D:/a/Fiona/Fiona/tests/data/coutwildrnp.zip/coutwildrnp.shp"
+    assert vsi_path.startswith("/vsizip/D")
+    assert vsi_path.endswith("coutwildrnp.zip/coutwildrnp.shp")
